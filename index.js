d575a2233fafc58e31f0e81ceea247639b70ddcd
Nitishh-kumar <nitish.26kumar2001@gmail.com>
2024-04-12 20:00:12 +0530

    Updated css file

--- index.js
+++ index.js
@@ -91,11 +91,13 @@ function renderWeatherInfo(weatherInfo){
     const cloudness=document.querySelector("[data-cloudiness]");
 
     // fetch values from weatherInfo object and put in ui elements
+    console.log(weatherInfo);
+
     cityName.innerText=weatherInfo?.name;
     countryIcon.src=`https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}ua.png`;
     desc.innerText=weatherInfo?.weather?.[0]?.description;
     weatherIcon.src=`https://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    temp.innerText=weatherInfo?.main?.temp;
+    temp.innerText=`${weatherInfo?.main?.temp} °C`;
     windspeed.innerText=weatherInfo?.wind?.speed;
     humidity.innerText=weatherInfo?.main?.humidity;
     cloudness.innerText=weatherInfo?.clouds?.all;
